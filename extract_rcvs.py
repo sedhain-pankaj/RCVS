74f0edc1b1a765cca40aa0901be2870436f66b7a
Pankaj Sedhain <kalpanakarki.ps@gmail.com>
2024-03-17 09:49:05 +1100

    imported new refactored files

--- extract_rcvs.py
+++ extract_rcvs.py
@@ -1,10 +1,8 @@
 from utils.pages_to_parse import pages_to_parse  # to get the start and end page numbers
 from utils.save_to_csv import save_to_csv  # to save the results to a CSV file
 from utils.constants import BASE_URL  # to get the base URL and clinic details
-from utils.parse_clinics_details import (
-    get_soup,
-    process_clinics,
-)  # to get the clinic details
+from utils.view_save_clinics_details import process_clinics  # to process the clinic details
+from utils.url_parser_bs4 import get_parsed_URL  # to get the parsed URL
 
 
 # main function
@@ -18,12 +16,12 @@ def main():
     # iterate over the pages from start page to end page
     for page_number in range(start, end + 1):
         url = f"{BASE_URL}/find-a-vet-practice/?filter-choice=&filter-keyword=&filter-searchtype=practice&filter-pss=true&p={page_number}"
-        soup = get_soup(url)  # call the get_soup function
-        if soup is None:
-            continue  # skip to the next page if the soup is None
+        url_contents = get_parsed_URL(url)  # get the parsed URL from bs4
+        if url_contents is None:
+            continue  # skip to the next page if the URL contents are not available
 
         # find all divs with class 'practice'
-        practices = soup.find_all("div", class_="practice")
+        practices = url_contents.find_all("div", class_="practice")
 
         # process the clinic details for each page
         process_clinics(practices, page_number, file_name)
@@ -35,6 +33,6 @@ def main():
         print("\nTask completed. Only viewing results. Save to CSV was not requested.")
 
 
-# run the main function if called from the command line
+# run the main function if only called from the command line
 if __name__ == "__main__":
     main()
