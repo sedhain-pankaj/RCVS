f0a7b84d77f844f8800bc3d3fb752d1473b0bf4f
Pankaj Sedhain <kalpanakarki.ps@gmail.com>
2024-03-17 10:03:39 +1100

    added more verbose comment

--- extract_rcvs.py
+++ extract_rcvs.py
@@ -7,10 +7,10 @@ from utils.url_parser_bs4 import get_parsed_URL  # to get the parsed URL
 
 # main function
 def main():
-    # start and end page numbers
+    # ask for the start and end page numbers
     start, end = pages_to_parse()
 
-    # call the save_To_CSV function and store the file name
+    # ask if the results should be saved to a CSV file
     file_name = save_to_csv()
 
     # iterate over the pages from start page to end page
@@ -20,13 +20,13 @@ def main():
         if url_contents is None:
             continue  # skip to the next page if the URL contents are not available
 
-        # find all divs with class 'practice'
+        # find all divs with class 'practice' i.e. individual clinic divs
         practices = url_contents.find_all("div", class_="practice")
 
-        # process the clinic details for each page
+        # process the clinic details for each page. print the results and save to a CSV file if requested
         process_clinics(practices, page_number, file_name)
 
-    # print a message depending on whether the results were saved to a CSV file
+    # print a message based on what was returned by the save_to_csv() function
     if file_name:
         print(f"\nTask completed. Results saved to {file_name}.")
     else:
